4422c91e6d3d2aaf1052e8bd5e965fdb6444a848
Justin Neff <justin@gatewayapps.com>
2019-05-06 16:43:43 -0500

    Tests for calculating calendar start and end

---
+++ src/lib/calendarUtils.ts
@@ -0,0 +1,15 @@
+import { addWeeks, endOfWeek, startOfDay, startOfMonth, startOfWeek } from 'date-fns'
+import { DayOfWeek } from './DayOfWeek'
+
+export function getCalendarStartDate(date: Date, weekStartsOn: DayOfWeek = DayOfWeek.Sunday): Date {
+  return startOfWeek(startOfMonth(date), { weekStartsOn })
+}
+
+export function getCalendarEndDate(
+  date: Date,
+  numberOfWeeks: number,
+  weekStartsOn: DayOfWeek = DayOfWeek.Sunday
+): Date {
+  const startDate = getCalendarStartDate(date, weekStartsOn)
+  return startOfDay(endOfWeek(addWeeks(startDate, numberOfWeeks - 1)))
+}
---
+++ tests/calendarUtils.spec.ts
@@ -0,0 +1,44 @@
+import { getCalendarEndDate, getCalendarStartDate } from '../src/lib/calendarUtils'
+import { DayOfWeek } from '../src/lib/DayOfWeek'
+
+describe('getCalendarMonthStartDate', () => {
+  it('Should return 2/24/2019 when date is 3/15/2019', () => {
+    expect(getCalendarStartDate(new Date(2019, 2, 15))).toEqual(new Date(2019, 1, 24))
+  })
+
+  it('Should return 2/24/2019 when date is 3/1/2019', () => {
+    expect(getCalendarStartDate(new Date(2019, 2, 1))).toEqual(new Date(2019, 1, 24))
+  })
+
+  it('Should return 2/24/2019 when date is 3/31/2019', () => {
+    expect(getCalendarStartDate(new Date(2019, 2, 31))).toEqual(new Date(2019, 1, 24))
+  })
+
+  it('Should return 2/25/2019 when date is 3/15/2019 and week starts on Monday', () => {
+    expect(getCalendarStartDate(new Date(2019, 2, 15), DayOfWeek.Monday)).toEqual(
+      new Date(2019, 1, 25)
+    )
+  })
+
+  it('Should return 2/25/2019 when date is 3/1/2019 and week starts on Monday', () => {
+    expect(getCalendarStartDate(new Date(2019, 2, 1), DayOfWeek.Monday)).toEqual(
+      new Date(2019, 1, 25)
+    )
+  })
+
+  it('Should return 2/25/2019 when date is 3/31/2019 and week starts on Monday', () => {
+    expect(getCalendarStartDate(new Date(2019, 2, 31), DayOfWeek.Monday)).toEqual(
+      new Date(2019, 1, 25)
+    )
+  })
+})
+
+describe('getCalendarEndDate', () => {
+  it('Should return 5/11/2019 when date is 4/15/2019 and number of weeks is 6', () => {
+    expect(getCalendarEndDate(new Date(2019, 3, 15), 6)).toEqual(new Date(2019, 4, 11))
+  })
+
+  it('Should return 4/27/2019 when date is 4/15/2019 and number of weeks is 4', () => {
+    expect(getCalendarEndDate(new Date(2019, 3, 15), 4)).toEqual(new Date(2019, 3, 27))
+  })
+})
