fa56b0ba963a4726f2869514b9ea061e01dd97a4
Justin Neff <justin@gatewayapps.com>
2019-05-08 10:07:28 -0500

    Fix broken test of Calendar component

--- tests/Calendar.spec.tsx
+++ tests/Calendar.spec.tsx
@@ -1,10 +1,10 @@
 import { mount } from 'enzyme'
 import React from 'react'
-import { Calendar } from '../src/components/Calendar'
+import Calendar from '../src/components/Calendar'
 
 describe('<Calendar />', () => {
   it('Should render properly', () => {
-    const wrapper = mount(<Calendar />)
-    expect(wrapper.text()).toContain('Hello from the calendar')
+    const wrapper = mount(<Calendar defaultDate={new Date(2019, 4, 8)} />)
+    expect(wrapper.text()).toContain('May 2019')
   })
 })
